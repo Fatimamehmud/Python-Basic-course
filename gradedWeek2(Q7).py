7253f42c194c95684dc4e945cff7f35ba3983350
prof "F.M.P <107954137+Fatimamehmud@users.noreply.github.com>
2023-10-13 00:50:24 +0300

    Create  gradedWeek2(Q7).py

---
+++ gradedWeek2(Q7).py
@@ -0,0 +1,5 @@
+#Create an empty string and assign it to the variable lett.  using
+#1) range, write code such that when your code is run, lett has 7 b’s ("bbbbbbb").
+lett=" "
+lett="b"*7
+print('\" '+lett +'\" ')
