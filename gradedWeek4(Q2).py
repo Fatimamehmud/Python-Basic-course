93dccaec9251a68e7563affd7abd73effe193327
prof "F.M.P <107954137+Fatimamehmud@users.noreply.github.com>
2023-10-15 20:28:14 +0300

    Create gradedWeek4(Q2).py

---
+++ gradedWeek4(Q2).py
@@ -0,0 +1,3 @@
+# Write code to take ‘London’ out of the list trav_dest.
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'London', 'Melbourne']
+trav_dest.pop(7)
